0d30b3e27f3799c28d770ec4bdbed21571ee8b62
hanmantguddapure <hanmantguddapure@gmail.com>
2022-10-07 22:06:34 +0530

    initial commit

--- src/main/java/com/flink/kafkaflinkintegration/KafkaFlinkIntegrationApplication.java
+++ src/main/java/com/flink/kafkaflinkintegration/KafkaFlinkIntegrationApplication.java
@@ -3,11 +3,24 @@ package com.flink.kafkaflinkintegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.flink.kafkaflinkintegration.config.FlinkKafkaReceiver;
+
 @SpringBootApplication
 public class KafkaFlinkIntegrationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaFlinkIntegrationApplication.class, args);
+		try {
+			try {
+				FlinkKafkaReceiver.StramConsumrer("flink-test", "localhost:9092");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }

fffdfec84dcf67161fa76bb04586d21021512dc4
hanmantguddapure <hanmantguddapure@gmail.com>
2022-10-07 21:03:09 +0530

    intial commit

---
+++ src/main/java/com/flink/kafkaflinkintegration/KafkaFlinkIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.flink.kafkaflinkintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaFlinkIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaFlinkIntegrationApplication.class, args);
+	}
+
+}
